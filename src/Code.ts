471b27e69568afee383cd74957c04729b707afe3
Oliver Allen-Cummings <olivercreativeworks@gmail.com>
2023-01-13 06:17:38 -0500

    init

---
+++ src/Code.ts
@@ -0,0 +1,7 @@
+// create new folder for project
+// run npm init
+// clone project 
+function myFunction() {
+  Logger.log("Hello, World")
+  Logger.log("Bye")
+}
---
+++ src/funcs.ts
@@ -0,0 +1,6 @@
+function happyPeople(){
+    Logger.log(4 + 4)
+    Logger.log("Making changes")
+    Logger.log("More changes")
+    SpreadsheetApp.getActiveSpreadsheet()
+}
